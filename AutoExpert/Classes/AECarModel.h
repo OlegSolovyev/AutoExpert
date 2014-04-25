f8e7b3289f0866dbd9beee96782af80e724a6247
Oleg Solovyev <Oleg.Solovyev@opensoftdev.ru>
2014-04-25 17:46:13 +0400

    d

---
+++ AutoExpert/Classes/AECarModel.h
@@ -0,0 +1,13 @@
+//
+//  AECarModel.h
+//  AutoExpert
+//
+//  Created by Oleg Solovyev on 27/03/14.
+//  Copyright (c) 2014 Opensoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AECarModel : NSObject
+
+@end
---
+++ AutoExpert/Classes/AECarModel.m
@@ -0,0 +1,13 @@
+//
+//  AECarModel.m
+//  AutoExpert
+//
+//  Created by Oleg Solovyev on 27/03/14.
+//  Copyright (c) 2014 Opensoft. All rights reserved.
+//
+
+#import "AECarModel.h"
+
+@implementation AECarModel
+
+@end
